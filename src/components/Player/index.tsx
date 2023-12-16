4e1c9a79245d634f70eb5f83d12c08a7070440aa
shoonia <alexanderz@wix.com>
2023-12-16 17:47:27 +0200

    Add Play/Pause button

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,8 +1,17 @@
-import type { RefCallback } from 'jsx-dom-runtime';
+import { type RefCallback, useRef, useText } from 'jsx-dom-runtime';
 
+import { _audio, _btn } from './styles.module.css';
 import { connect, dispatch } from '../../store';
 
+const enum LABEL {
+  PLAY = 'Play',
+  PAUSE = 'Pause',
+}
+
 export const Player: JSX.FC = () => {
+  const [label, setLabel] = useText(LABEL.PLAY);
+  const button = useRef<HTMLButtonElement>();
+
   const ready: RefCallback<HTMLAudioElement> = (audio) => {
     let i: number;
 
@@ -11,16 +20,33 @@ export const Player: JSX.FC = () => {
 
     audio.addEventListener('play', () => {
       i = setInterval(setTime, 1000);
+      setLabel(LABEL.PAUSE);
     });
 
     audio.addEventListener('pause', () => {
       clearInterval(i);
       setTime();
+      setLabel(LABEL.PLAY);
+    });
+
+    audio.addEventListener('canplay', () => {
+      button.current.disabled = false;
+      setLabel(LABEL.PLAY);
     });
 
     connect('url', (state) => {
       audio.src = state.url;
       audio.currentTime = state.time;
+    });
+
+
+    const disconnect = connect(() => {
+      button.current.addEventListener('click', () => {
+        if (audio.paused) audio.play();
+        else audio.pause();
+      });
+
+      disconnect();
     });
   }
 
@@ -28,9 +54,16 @@ export const Player: JSX.FC = () => {
     <div>
       <audio
         ref={ready}
+        class={_audio}
         preload="metadata"
         controls
+        controlsList="nodownload"
       />
+      <div>
+        <button ref={button} class={_btn} type="button" disabled>
+          {label}
+        </button>
+      </div>
     </div>
   )
 }
