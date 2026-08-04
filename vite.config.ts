f815b70472891d270d766a499ab29fd85ce1bd89
shoonia <alexanderz@wix.com>
2026-08-04 22:37:15 +0300

    Refactor code structure for improved readability and maintainability

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig, UserConfig } from 'vite';
+import babel from '@rolldown/plugin-babel';
+import generateScopedName from 'mini-css-class-name/postcss-modules';
+
+export default defineConfig(({ mode }): UserConfig => {
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  return {
+    root: 'src',
+    base: '/audio-player/',
+    server: { open: true },
+    build: {
+      outDir: '../dist',
+      assetsDir: '.',
+      emptyOutDir: true,
+      target: 'esnext',
+      cssCodeSplit: false,
+      minify: isProd ? 'terser' : false,
+      sourcemap: isDev,
+      modulePreload: false,
+      reportCompressedSize: true,
+      rolldownOptions: {
+        treeshake: false,
+      },
+    },
+    css: {
+      modules: {
+        generateScopedName: isProd
+          ? generateScopedName()
+          : '[name]__[local]___[hash:base64:5]',
+      },
+    },
+    oxc: {
+      jsx: 'preserve',
+      target: 'esnext',
+    },
+    plugins: [
+      babel({
+        include: /\.tsx$/,
+        comments: isDev,
+        sourceMap: isDev,
+        presets: ['jsx-dom-runtime/babel-preset'],
+      }),
+    ],
+  };
+});
